34a8f251a9eeb86f3d1ca15718afb9e1d2b5e794
frunoob <3024446880@qq.com>
2020-12-15 01:51:09 +0800

    init

---
+++ scripts/homework_1.js
@@ -0,0 +1,40 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        top_text:'网易云音乐',
+        tabtitle:["推荐歌曲","热歌榜","搜索"],
+        middle_top_text:'推荐歌单',
+        text2:'最新音乐',
+        text:[
+             'One Time'
+            ,'Change Me'
+            ,'Baby'
+            ,'Somebody To Love'
+            ,'Never Say Never'
+            ,'purpose'
+
+        ],
+        img:'https://gitee.com/frunoob/blog-image/raw/master/image/20201215002405.jpg',
+        num:[
+             "2"
+             ,"4"
+             ,"8"
+             ,"16"
+             ,"32"
+             ,"65"
+        ],
+        song_title:[
+            'lonely'
+            ,'never say never'
+            ,'frist dance'
+            ,'you smile'
+            ,'never let you go'
+            ,'kiss and tell'
+            ,'dr bieber'
+            ,'rich girl'
+            ,'love me'
+            ,'down to earth'
+        ],
+        singer:'justin bieber'
+    }
+})
